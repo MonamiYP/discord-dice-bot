22a299ab3d745b60b923dd4f0bc7bc43e22ad29f
MonamiYP <monami.yoshioka@gmail.com>
2022-09-01 17:18:36 +0200

    Created file for !randchar command

--- src/commands/randchar.js
+++ src/commands/randchar.js
@@ -1,10 +1,18 @@
-const { rollDice } = require('../modules/dice.js');
+const { rollDice } = require("../modules/dice.js");
 
 function randchar(message) {
-    const sorted_roll_values = rollDice(4, 6, 0).sort();
-    const smallest_roll = sorted_roll_values.shift();
-    const reply_message = `Generated random stats:\n[${sorted_roll_values}] (deleted ${smallest_roll})`;
+    let reply_message = `Generated random stats:\n`;
+    for (let i = 0; i < 6; i++) {
+        const sorted_roll_values = rollDice(4, 6, 0).sort();
+        const smallest_roll = sorted_roll_values.shift();
+        const sum = sorted_roll_values.reduce(
+            (partialSum, number) => partialSum + number, 0
+        );
+        reply_message = reply_message.concat(
+            `[${sorted_roll_values},~~${smallest_roll}~~] = **${sum}**\n`
+        );
+    }
     message.reply(reply_message);
 }
 
-module.exports = randchar
+module.exports = randchar;
