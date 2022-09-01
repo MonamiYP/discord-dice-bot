baa81d122db26a8594c0fd4e6b7b23101f0b9df6
MonamiYP <monami.yoshioka@gmail.com>
2022-09-01 12:48:07 +0200

    Formatted command to be roll+modifier instead of roll +modifier

--- src/events/roll.js
+++ src/events/roll.js
@@ -9,15 +9,17 @@ module.exports = {
             try {
                 let reply_message = "The format of your roll command is not correct";
                 const dice_arguments = message.content.split(' ');
-                const dice_number_value = dice_arguments[1].split('d');
-                const number = parseInt(dice_number_value[0]);
-                const value = parseInt(dice_number_value[1]);
+                const dice_roll_modifier = dice_arguments[1].split('+'); 
+                const roll_number_value = dice_roll_modifier[0].split('d');
+                const number = parseInt(roll_number_value[0]);
+                const value = parseInt(roll_number_value[1]);
                 let modifier = 0;
             
-                if (dice_arguments.length == 3) {
-                    modifier = parseInt(dice_arguments[2].slice(1));
-                } else if (dice_arguments.length != 2) {
-                    return
+                if (dice_arguments.length == 2) {
+                    modifier = parseInt(dice_roll_modifier[1]);
+                    if (Number.isNaN(modifier)) {
+                        modifier = 0;
+                    }
                 }
 
                 const roll_values = rollDice(number, value, modifier).toString();
